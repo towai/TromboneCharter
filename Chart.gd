fb3d393d75c353297a2c131e6fca6d851e46a052
twi <shado0909@gmail.com>
2022-11-05 23:35:19 -0500

    no need for this to be 2 lines

--- Chart.gd
+++ Chart.gd
@@ -170,16 +170,14 @@ func find_touching_notes(the_note:Note) -> Dictionary:
 
 func get_matching_note_on(time:float, exclude:Array = []): # -> Note or null
 	for note in get_children():
-		if !(note is Note): continue
-		if note in exclude: continue
+		if !(note is Note) || (note in exclude): continue
 		if abs(note.bar - time) < 0.01: return note
 	return null
 
 
 func get_matching_note_off(time:float, exclude:Array = []): # -> Note or null
 	for note in get_children():
-		if !(note is Note): continue
-		if note in exclude: continue
+		if !(note is Note) || (note in exclude): continue
 		if abs(note.end - time) < 0.01: return note
 	return null
 
