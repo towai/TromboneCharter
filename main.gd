c789306c107f7d3b7573f30a19a5c5a87232a6af
twi <shado0909@gmail.com>
2023-09-16 17:13:28 -0500

    Fix error causing no tmb initialization on first launch, reorder functions, amend ogg convert logic & prints

--- main.gd
+++ main.gd
@@ -19,7 +19,6 @@ func _ready():
 	if err:
 		print("Couldn't load config: %s" % error_string(err))
 		show_popup($Instructions) # probably first load
-		return
 	
 	var argv : PackedStringArray = OS.get_cmdline_args()
 	if !argv.is_empty() && argv[0].ends_with(".tmb"):
@@ -31,9 +30,16 @@ func _ready():
 		_on_load_dialog_file_selected(path)
 		return
 	
-	$LoadDialog.current_dir = cfg.get_value("Config","saved_dir")
+	$LoadDialog.current_dir = cfg.get_value("Config","saved_dir") if !err else "."
 	
 	_on_new_chart_confirmed()
+
+
+func _input(event):
+	event = event as InputEventKey
+	if event == null: return
+	if event.pressed && event.keycode == KEY_S && Input.is_key_pressed(KEY_CTRL):
+		_on_save_chart_pressed()
 
 
 func _on_description_text_changed(): tmb.description = %Description.text
@@ -46,6 +52,41 @@ func _on_help_button_pressed(): show_popup($Instructions)
 func show_popup(window:Window):
 	window.position = popup_location
 	window.show()
+
+
+func try_to_load_wav(path:String) -> int:
+	print("Try load wav from %s" % path)
+	var f = FileAccess.open(path,FileAccess.READ)
+	if f == null:
+		var err = FileAccess.get_open_error()
+		return err
+	
+	var stream := AudioLoader.loadfile(path, false) as AudioStreamWAV
+	if stream == null :
+		print("stream null?")
+		return ERR_FILE_CANT_READ
+	elif stream.data == null || stream.data.is_empty():
+		print("no data?")
+		return ERR_FILE_CANT_READ
+	
+	%WavPlayer.stream = stream
+	return OK
+
+
+func try_to_convert_ogg(path:String) -> int:
+	var dir = path.substr(0,path.rfind("/"))
+	if dir == path: dir = path.substr(0,path.rfind("\\"))
+	var args = PackedStringArray([
+			"-i",
+			'%s' % path,
+			'%s' % (dir + "/song.wav")
+			])
+#	print(args)
+	var output = []
+	var err = OS.execute("ffmpeg",args,output,true,true)
+	print(output[0].c_unescape())
+	print(output.size())
+	return err
 
 
 func _on_new_chart_pressed(): show_popup($NewChartConfirm)
@@ -80,64 +121,24 @@ func _on_load_dialog_file_selected(path):
 		if err:
 			print("No wav loaded -- %s" % error_string(err))
 			if %Settings.convert_ogg:
-				print("Try to convert song.ogg")
+				print("try to convert song.ogg to wav")
 				DirAccess.open(dir)
 				err = DirAccess.get_open_error()
 				if err:
 					print("DirAccess error : %s" % error_string(err))
 					return
+				if !FileAccess.file_exists(dir + "/song.ogg"):
+					print("song.ogg not present in the tmb's folder")
+					return
 				err = try_to_convert_ogg(dir + "/song.ogg")
 				if !err:
 					var wav_err = try_to_load_wav(dir + "/song.wav")
-					if wav_err: print("no ffmpeg error but couldn't load wav??")
-				else: print("ogg->wav conversion result %d (-1 means ffmpeg was not found)"
-						% err)
+					if wav_err: print("ffmpeg success but couldn't load resulting wav?")
+				else: # redundant to print explanation, label tells us if ffmpeg's missing
+					print("conversion failed: " + error_string(err))
 	
 	%WAVLoadedLabel.text = "song.wav loaded!" if %WavPlayer.stream != null \
 			else "no ffmpeg!" if err == -1 else "no song.wav loaded!"
-	
-
-
-func try_to_load_wav(path:String) -> int:
-	print("Try load wav from %s" % path)
-	var f = FileAccess.open(path,FileAccess.READ)
-	if f == null:
-		var err = FileAccess.get_open_error()
-		return err
-	
-	var stream := AudioLoader.loadfile(path, false) as AudioStreamWAV
-	if stream == null :
-		print("stream null?")
-		return ERR_FILE_CANT_READ
-	elif stream.data == null || stream.data.is_empty():
-		print("no data?")
-		return ERR_FILE_CANT_READ
-	
-	%WavPlayer.stream = stream
-	return OK
-
-
-func try_to_convert_ogg(path:String) -> int:
-	var dir = path.substr(0,path.rfind("/"))
-	if dir == path: dir = path.substr(0,path.rfind("\\"))
-	var args = PackedStringArray([
-			"-i",
-			'%s' % path,
-			'%s' % (dir + "/song.wav")
-			])
-	print(args)
-	var output = []
-	var err = OS.execute("ffmpeg",args,output,true,true)
-	print(output[0].c_unescape())
-	print(output.size())
-	return err
-
-
-func _input(event):
-	event = event as InputEventKey
-	if event == null: return
-	if event.pressed && event.keycode == KEY_S && Input.is_key_pressed(KEY_CTRL):
-		_on_save_chart_pressed()
 
 
 func _on_save_chart_pressed():
@@ -146,23 +147,25 @@ func _on_save_chart_pressed():
 		_on_save_dialog_file_selected($SaveDialog.current_path)
 	else: show_popup($SaveDialog)
 
+# TODO Figure out if there have been Godot changes to make
+# all the windows-specific stuff unnecessary
 func _on_save_dialog_file_selected(path:String):
 	if path[1] != ':' && OS.get_name() == "Windows":
 		print("driveless path %s" % path)
 		var drive : String
 		var saved_dir = cfg.get_value("Config","saved_dir")
 		if saved_dir == null:
-			$Alert.alert("couldn't save! you tried to break it on purpose didn't you",
+			$Alert.alert("no saved dir in config! did you delete it?",
 					Vector2(12, %ViewSwitcher.global_position.y + 38),
 					Alert.LV_ERROR)
 			return
 			
 		if saved_dir[1] != ':':
-			print("Fuck shit!!")
+			print("couldn't get drive letter from saved directory in cfg file!")
 			return
 		drive = saved_dir.substr(0,saved_dir.find('/'))
 		if drive == saved_dir:
-			print("Cock backslash")
+			print("Saved dir in cfg uses backslashes -- user manually edited cfg file?")
 			drive = saved_dir.substr(0,saved_dir.find('\\'))
 		path = drive + path
 	var err = tmb.save_to_file(path,$SaveDialog.current_dir)
@@ -203,6 +206,7 @@ func _on_copy_button_pressed():
 		_on_copy_confirmed()
 		return
 	$CopyConfirm.show()
+
 
 func _on_copy_confirmed():
 	var start = Global.settings.section_start
